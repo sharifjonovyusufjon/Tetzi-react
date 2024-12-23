1c309db3df3a3d2550b33af8fb765c24ecda44f4
sharifjonovyusufjon <Yusufjon6727@gmail.com>
2024-12-23 22:02:41 +0900

    feat: develop routing sistem

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,10 @@
 import { Routes, Route, Link } from "react-router-dom";
 import HomePage from "./screens/homePage";
 import ShopPage from "./screens/shopPage";
+import AboutPage from "./screens/aboutPage";
+import ContactsPage from "./screens/contactsPage";
+import JournalPage from "./screens/journalPage";
+import UserPage from "./screens/userPage";
 
 export default function App() {
   return (
@@ -13,12 +17,28 @@ export default function App() {
           <li>
             <Link to="/shop">ShopPage</Link>
           </li>
+          <li>
+            <Link to="/about">AboutPage</Link>
+          </li>
+          <li>
+            <Link to="/contacts">ContactsPage</Link>
+          </li>
+          <li>
+            <Link to="/journal">JournalPage</Link>
+          </li>
+          <li>
+            <Link to="/user">UserPage</Link>
+          </li>
         </ul>
       </nav>
 
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contacts" element={<ContactsPage />} />
+        <Route path="/journal" element={<JournalPage />} />
+        <Route path="/user" element={<UserPage />} />
       </Routes>
     </div>
   );
---
+++ src/app/screens/journalPage/index.tsx
@@ -0,0 +1,3 @@
+export default function JournalPage() {
+  return <div>JournalPage</div>;
+}
