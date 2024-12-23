4f5c6f1b020b9866c5a946f1470b0c28bc5458db
sharifjonovyusufjon <Yusufjon6727@gmail.com>
2024-12-24 07:02:37 +0900

    feat: publishing home shop categories

--- src/app/screens/homePage/shopCategories.tsx
+++ src/app/screens/homePage/shopCategories.tsx
@@ -1,3 +1,52 @@
+import { Box, Container, Stack } from "@mui/material";
+import FirstPageIcon from "@mui/icons-material/FirstPage";
+import LastPageIcon from "@mui/icons-material/LastPage";
+
 export default function ShopCategories() {
-  return <div>ShopCategories</div>;
+  const shopCategories = [
+    {
+      productCategory: "CAR SEAT",
+      productImage: "/img/baby.jpg",
+    },
+    {
+      productCategory: "STROLLER",
+      productImage: "/img/baby.jpg",
+    },
+    {
+      productCategory: "CLOTHES",
+      productImage: "/img/baby.jpg",
+    },
+    {
+      productCategory: "SHOES",
+      productImage: "/img/baby.jpg",
+    },
+  ];
+
+  return (
+    <div className="shop-categories">
+      <Container className="shop-categories-container">
+        <Box className={"shop-categories-title"}>Shop by Categories</Box>
+        <Stack className="shop-categories-menu">
+          {shopCategories.map((ele, index) => {
+            return (
+              <Stack className="shop-box" key={index}>
+                <Box className={"shop-image-box"}>
+                  <img className="img" src={ele.productImage} alt="" />
+                </Box>
+                <Box className={"text"}>{ele.productCategory}</Box>
+              </Stack>
+            );
+          })}
+        </Stack>
+        <Stack className="shop-pagination">
+          <Box className={"pagination"}>
+            <FirstPageIcon sx={{ color: "rgb(255, 255, 255)" }} />
+          </Box>
+          <Box className={"pagination"}>
+            <LastPageIcon sx={{ color: "rgb(255, 255, 255)" }} />
+          </Box>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
