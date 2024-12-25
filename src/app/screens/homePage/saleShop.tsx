47267a403b52f48c3851baea0c8cdb735439e6c3
sharifjonovyusufjon <Yusufjon6727@gmail.com>
2024-12-25 23:41:48 +0900

    publishing saleShop home components

--- src/app/screens/homePage/saleShop.tsx
+++ src/app/screens/homePage/saleShop.tsx
@@ -1,3 +1,20 @@
+import { Box } from "lucide-react";
+
 export default function SaleShop() {
-  return <div>SaleShop</div>;
+  return (
+    <div className="sale-shop">
+      <div className="sale-shop-div">
+        <div className="left">
+          <div className="left-text">SALE</div>
+        </div>
+        <div className="right">
+          <div className="right-box">
+            <div className="box-title">Final Offer for Summer Items!</div>
+            <div className="box-text">Now up to 70% off</div>
+            <div className="box-button">Shop</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
