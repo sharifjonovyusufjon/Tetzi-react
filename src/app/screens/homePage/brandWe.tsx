bb745ae3cbc063cc6a7470763e78eb60f530615a
sharifjonovyusufjon <Yusufjon6727@gmail.com>
2024-12-25 23:59:49 +0900

    publishing brandWe home components

--- src/app/screens/homePage/brandWe.tsx
+++ src/app/screens/homePage/brandWe.tsx
@@ -1,3 +1,21 @@
 export default function BrandWe() {
-  return <div>BrendWe</div>;
+  return (
+    <div className="brands-we">
+      <div className="brands-container">
+        <div className="brand-title">Brands We Love</div>
+        <div className="brand-text">
+          “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore tempor sed do magna
+          aliqua.”
+        </div>
+        <div className="brand-box">
+          <img src="/icons/brand-baks.jpeg" alt="" />
+          <img src="/icons/brand-blue.jpeg" alt="" />
+          <img src="/icons/brand-balkan.jpeg" alt="" />
+          <img src="/icons/brand-teal.jpeg" alt="" />
+          <img src="/icons/brand-adveist.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
